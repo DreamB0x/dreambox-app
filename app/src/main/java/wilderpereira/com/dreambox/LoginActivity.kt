925045f959bd1df642c94f68eae7dead545d0f9e
Wilder Pereira <wilder_roberto@hotmail.com>
2017-12-02 15:55:01 -0200

    add login screen

---
+++ app/src/main/java/wilderpereira/com/dreambox/LoginActivity.kt
@@ -0,0 +1,19 @@
+package wilderpereira.com.dreambox
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+
+    fun doLogin(view: View) {
+
+    }
+
+}
