346d4a41d24b9d079ef40322dc43a28d01f22dad
Wilder Pereira <wilder_roberto@hotmail.com>
2017-12-02 20:51:39 -0200

    add installments/price activity

---
+++ app/src/main/java/wilderpereira/com/dreambox/InstallmentsActivity.kt
@@ -0,0 +1,12 @@
+package wilderpereira.com.dreambox
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class InstallmentsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_installments)
+    }
+}
